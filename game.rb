f8b7a46b55705dfe98e359316f4a236aa96d08a4
hykare <78385964+hykare@users.noreply.github.com>
2022-02-26 22:18:43 +0100

    use getters

--- game.rb
+++ game.rb
@@ -4,7 +4,7 @@ class Game
   VICTORY_MSG = 'Congratulations! You broke the code!'.freeze
   FAILURE_MSG = 'Aw, maybe next time'.freeze
   TOTAL_ROUNDS = 12
-  attr_reader :board, :round
+  attr_reader :board, :round, :code
 
   def initialize
     @board = Board.new
@@ -13,14 +13,14 @@ class Game
   end
 
   def play
-    print 'pssst! the secret code is ', @code, "\n\n"
+    print 'pssst! the secret code is ', code, "\n\n"
     while round < TOTAL_ROUNDS
       puts "round #{round + 1}"
       board.draw(round)
       puts INSTRUCTIONS
       guess = get_guess
-      board.update(guess, round, @code)
-      break if guess == @code
+      board.update(guess, round, code)
+      break if guess == code
 
       @round += 1
     end
@@ -28,8 +28,8 @@ class Game
 
   def final_screen
     board.draw(round)
-    puts FAILURE_MSG if @round == TOTAL_ROUNDS
-    puts VICTORY_MSG if @round < TOTAL_ROUNDS
+    puts FAILURE_MSG if round == TOTAL_ROUNDS
+    puts VICTORY_MSG if round < TOTAL_ROUNDS
   end
 
   def get_guess
