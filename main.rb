ee818ade59b2ae94750ff63e230d3f884513ccda
hykare <78385964+hykare@users.noreply.github.com>
2022-03-06 23:35:34 +0100

    choose game mode at start of game

--- main.rb
+++ main.rb
@@ -1,9 +1,24 @@
 $LOAD_PATH << '.'
 require 'game'
 
+def choose_game_mode
+  puts 'Play as:'
+  puts '1 codebreaker'
+  puts '2 codemaker'
+  game_mode = gets.chomp.to_i
+  case game_mode
+  when 1
+    Game.new
+  when 2
+    GameAsCodeMaker.new
+  else
+    Game.new
+  end
+end
+
 game_on = true
 while game_on
-  game = Game.new
+  game = choose_game_mode
   game.play
   game.final_screen
   puts 'Would you like to play again? y/n'
